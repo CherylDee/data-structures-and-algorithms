e1ab388b320465edc7e4c38e3447cb4f76bb379f
Cheryl Dee <cdmccalop@gmail.com>
2023-03-18 23:46:22 -0700

    challenge 15

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -11,7 +11,7 @@ Write a function named screenForNames that takes in an array of strings and uses
 ------------------------------------------------------------------------------------------------ */
 
 const screenForNames = (arr) => {
-  // Solution code here...
+ let regex = /^(Mr\.|Mrs\.|Ms\.|Dr\.)\s[a-zA-Z]+/
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -120,7 +120,7 @@ const sortBy = (property, arr) => {
 };
 
 /* ------------------------------------------------------------------------------------------------
-CHALLENGE 5 
+CHALLENGE 5
 
 Write a function that determines if a given URL is secure, beginning with https://
 
@@ -136,7 +136,7 @@ const isSecure = (url) => {
 };
 
 /* ------------------------------------------------------------------------------------------------
-CHALLENGE 6 
+CHALLENGE 6
 
 Write a function named detectTicTacToeWin that accepts a two-dimensional array of strings. Each string is guaranteed to be either "X", "O" or an empty string. Your function should check to see if any row, column, or either diagonal direction has three matching "X" or "O" symbols (non-empty strings), three-in-a-line.
 
